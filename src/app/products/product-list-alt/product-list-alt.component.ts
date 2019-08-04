5f0f4fbf7bce3b2180ed3e70f8b08eee43f11422
James Alexander <james1081@hotmail.com>
2019-08-04 11:22:05 -0400

    Initial Commit

---
+++ src/app/products/product-list-alt/product-list-alt.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { EMPTY } from 'rxjs';
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { catchError } from 'rxjs/operators';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list-alt.component.html'
+})
+export class ProductListAltComponent {
+  pageTitle = 'Products';
+  errorMessage = '';
+  selectedProductId;
+  products$ = this.productService.productsWithCategory$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
+
+  constructor(private productService: ProductService) { }
+
+  onSelected(productId: number): void {
+    console.log('Not yet implemented');
+  }
+}
